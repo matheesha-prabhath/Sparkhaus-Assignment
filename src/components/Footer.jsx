847ec55e8a94f88b392a30aa2d31369b6879db90
matheesha-prabhath <prabhathmatheesha@gmail.com>
2024-10-02 22:05:57 +0530

    Add Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 p-6">
+      <div className="container mx-auto text-center text-white">
+        <p>&copy; 2024 Company Name. All Rights Reserved.</p>
+        <p>123 Business Street, City, Country</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
